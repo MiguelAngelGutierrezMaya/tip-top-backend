91c248d802a993d844af69571a30e46c6b296c4e
Sebastian Arias <sarias@pcaingenieria.com>
2021-02-22 18:14:10 -0500

    Se agrega la columna de Nivel en el panel admin de las Lecciones

--- tip_top_backend/lessons/admin/lesson_admin.py
+++ tip_top_backend/lessons/admin/lesson_admin.py
@@ -10,6 +10,11 @@ from tip_top_backend.lessons.models import Lesson
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     """Lesson admin."""
-    list_display = ('title', 'is_last', 'parent', 'unit')
-    search_fields = ('title', 'is_last', 'parent', 'unit')
-    list_filter = ('title', 'is_last', 'parent', 'unit')
+    list_display = ('title', 'is_last', 'parent', 'unit', 'level')
+    search_fields = ('title', 'is_last', 'parent', 'unit', 'level')
+    list_filter = ('title', 'is_last', 'parent', 'unit', 'unit__level__name')
+
+    def level(self, obj):
+        return obj.unit.level
+
+    level.admin_order_field = 'unit__level__name'
